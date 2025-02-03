f660669acb0fb4677efc743cc762cc81c9921a2b
Rex342645 <vikasrautela88@gmail.com>
2025-02-03 22:46:52 +0530

    skeleton routes

--- index.js
+++ index.js
@@ -2,3 +2,30 @@ const express=require('express');
 const jwt=require('jsonwebtoken');
 const app=express();
 app.use(express.json());
+
+app.post("/user/signup",(req,res)=>{
+    res.json({
+        msg:"signup endpoint"
+    })
+})
+app.post("/user/signin",(req,res)=>{
+    res.json({
+        msg:"signin endpoint"
+    })
+})
+app.post("/user/purchases",(req,res)=>{
+    res.json({
+        msg:"signin endpoint"
+    })
+})
+app.post("/course/purchase",(req,res)=>{
+    res.json({
+        msg:"signin endpoint"
+    })
+})
+app.get("/user/courses",(req,res)=>{
+    res.json({
+        msg:"login endpoint"
+    })
+})
+app.listen(3000)
